62379ea72f0f1cfcc124381dc4fb286e70c48520
Kyle Getrost <1052366+kyleget@users.noreply.github.com>
2023-12-27 16:40:43 +0700

    fix: upgrade Expo (#57)
    * chore: upgrade
    
    * chore: add changeset

--- apps/storybook/.storybook/main.js
+++ apps/storybook/.storybook/main.js
@@ -1,10 +1,10 @@
 module.exports = {
   stories: ['../stories/*.stories.?(ts|tsx|js|jsx)'],
   addons: [
-    '@storybook/addon-ondevice-notes',
+    /*'@storybook/addon-ondevice-notes',
     '@storybook/addon-ondevice-controls',
     '@storybook/addon-ondevice-backgrounds',
     '@storybook/addon-ondevice-actions',
-    'storybook-addon-ondevice-styled-theme',
+    'storybook-addon-ondevice-styled-theme',*/
   ],
 };
